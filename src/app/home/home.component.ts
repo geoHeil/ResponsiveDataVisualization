e215af160ca57b58e7b31ea6cdc51c2a22a2a81e
geoHeil <e1225063@student.tuwien.ac.at>
2016-06-29 08:33:23 +0200

    ADD some bar chart with error

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 import { AppState } from '../app.service';
 import { Title } from './title';
+import { BarGraph } from './bar-graph';
 import { XLarge } from './x-large';
+// import * as d3 from 'd3'
 
 @Component({
   // The selector is what angular internally uses
@@ -16,7 +18,7 @@ import { XLarge } from './x-large';
   // We need to tell Angular's compiler which directives are in our template.
   // Doing so will allow Angular to attach our behavior to an element
   directives: [
-    XLarge
+    XLarge, BarGraph
   ],
   // We need to tell Angular's compiler which custom pipes are in our template.
   pipes: [ ],
@@ -28,9 +30,13 @@ import { XLarge } from './x-large';
 export class Home {
   // Set our default values
   localState = { value: '' };
+  private graphData: Array<Number>;
   // TypeScript public modifiers
   constructor(public appState: AppState, public title: Title) {
 
+    // d3.select("body").transition()
+    //   .style("background-color", "black");
+    this.graphData = [10, 20, 30, 40, 60];
   }
 
   ngOnInit() {
